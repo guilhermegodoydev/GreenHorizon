5afc5dec6b5d53a6c0aea81ffd2f838354c85ae3
matheusmmls <matheusmmls2006@gmail.com>
2026-04-10 00:39:05 -0300

    feat: implement animated life HUD (debug input for damage test) and life management system

--- core/src/main/java/io/github/guilhermegodoydev/greenhorizon/Main.java
+++ core/src/main/java/io/github/guilhermegodoydev/greenhorizon/Main.java
@@ -1,29 +1,69 @@
 package io.github.guilhermegodoydev.greenhorizon;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import io.github.guilhermegodoydev.greenhorizon.itens.LifeManager;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
+    // PARTE DO MAPA
     private TiledMap mapa;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
     private FitViewport viewport;
 
+    // SISTEMA DE VIDA E HUD (HUD = Interface)
+    private SpriteBatch batch;
+    private BitmapFont fonte;
+    private LifeManager lifeManager;
+
+    // Imagens e Animação
+    private Texture imgBarra;
+    private Texture spriteSheetAnimacao;
+    private TextureRegion[] framesCoracao = new TextureRegion[4]; // 4 corações na tira
+
+    // Controle da Animação
+    private float tempoAnimacao = 0;
+    private boolean estaSofrendoDano = false;
+
     @Override
     public void create() {
+        // --- Setup do Mapa ---
         mapa = new TmxMapLoader().load("mapa.tmx");
         renderer = new OrthogonalTiledMapRenderer(mapa);
-
         camera = new OrthographicCamera();
         viewport = new FitViewport(320, 240, camera);
+
+        // Setup do Sistema de Vida
+        batch = new SpriteBatch();
+        fonte = new BitmapFont();
+        lifeManager = new LifeManager(10);
+
+        // Carregar imagens
+        imgBarra = new Texture("apenas_barra.png");
+        spriteSheetAnimacao = new Texture("animacao_perdendo_vida.png");
+
+        // Tirar o borrão das imagens (Pixel Art nítida)
+        imgBarra.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        spriteSheetAnimacao.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+
+        // Cortar a tira vertical em 4 pedaços iguais
+        int larguraFrame = spriteSheetAnimacao.getWidth() / 4;
+        int alturaFrame = spriteSheetAnimacao.getHeight();
+        for (int i = 0; i < 4; i++) {
+            framesCoracao[i] = new TextureRegion(spriteSheetAnimacao,i * larguraFrame, 0, larguraFrame, alturaFrame);
+        }
     }
 
     @Override
@@ -33,12 +73,50 @@ public class Main extends ApplicationAdapter {
         viewport.apply();
         renderer.setView(camera);
         renderer.render();
+
+        // Lógica e Desenho da Vida
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+
+        // 1. Desenha a barra de fundo
+        batch.draw(imgBarra, 250, 210, 60, 25);
+
+        // 2. Desenha o coração (Animado ou Parado)
+        int frameAtual = 0;
+        if (estaSofrendoDano) {
+            tempoAnimacao += Gdx.graphics.getDeltaTime();
+            frameAtual = (int)(tempoAnimacao / 0.1f); // Troca frame a cada 0.1s
+
+            if (frameAtual > 3) { // Acabou os 4 frames
+                estaSofrendoDano = false;
+                tempoAnimacao = 0;
+                frameAtual = 0;
+            }
+        }
+        batch.draw(framesCoracao[frameAtual], 255, 216, 16, 16);
+
+        // 3. Desenha o contador de vidas
+        fonte.draw(batch, "X " + lifeManager.getVidasAtuais(), 275, 228);
+
+        batch.end();
+
+        // --- Teste de Dano ---
+        if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
+            lifeManager.perderVida(1);
+            estaSofrendoDano = true;
+            tempoAnimacao = 0;
+        }
     }
 
     @Override
     public void dispose() {
+        // Limpar tudo
         mapa.dispose();
         renderer.dispose();
+        batch.dispose();
+        imgBarra.dispose();
+        spriteSheetAnimacao.dispose();
+        fonte.dispose();
     }
 
     @Override
