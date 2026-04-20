63a5ecce957be0c3949875b5f9ebf0b60a271f71
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-04-20 17:38:45 -0300

    refactor(towers): integrate TowerTree with centralized Assets system

--- core/src/main/java/io/github/guilhermegodoydev/greenhorizon/core/entities/towers/TowerTree.java
+++ core/src/main/java/io/github/guilhermegodoydev/greenhorizon/core/entities/towers/TowerTree.java
@@ -1,17 +1,16 @@
 package io.github.guilhermegodoydev.greenhorizon.core.entities.towers;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import io.github.guilhermegodoydev.greenhorizon.core.utils.Assets;
 
 public class TowerTree extends TowerBase {
 
     public TowerTree(float x, float y) {
-        super(new Sprite(new Texture("torre_arvore_nivel1.png")), x, y, 10f, 80f, 1.2f);
+        super(new Sprite(Assets.getTexture("torre_arvore_nivel1.png")), x, y, 10f, 80f, 1.2f);
     }
 
     @Override
     public void attack() {
         System.out.println("TowerTree está disparando folhas!");
-        cooldownTimer = 0;
     }
 }
